b5513549a69856850c6eda61e9c951ac43da39c1
infinith4 <infinith4@gmail.com>
2025-03-01 22:51:37 +0900

    m

--- wave03/dev-icp-hackathon/postCommand.sh
+++ wave03/dev-icp-hackathon/postCommand.sh
@@ -11,11 +11,16 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
 npm install -g npm@11.1.0
 nvm use --lts
 
-sudo apt-get update
-sudo apt-get install expect -y
+# sudo apt-get update
+sudo apt install libxtables12 -y
+# sudo apt-get install expect -y
 
 
-bash ./postCommandDfx.sh
+# bash ./postCommandDfx.sh
+export DFXVM_INIT_YES=true
+sh -ci "$(curl -fsSL https://internetcomputer.org/install.sh)"
+export DFX_VERSION=0.25.0
+sh -ci "$HOME/.local/share/dfx/env && dfx --version"
 
 
 dfx --version

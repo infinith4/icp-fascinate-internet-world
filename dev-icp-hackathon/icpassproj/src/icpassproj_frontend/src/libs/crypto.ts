d015f49be02b509e65e45fd29dc14b4d64481588
infinith4 <infinith4@gmail.com>
2025-03-25 23:45:52 +0900

    README追加

---
+++ dev-icp-hackathon/icpassproj/src/icpassproj_frontend/src/libs/crypto.ts
@@ -0,0 +1,140 @@
+import type { BackendActor } from './actor';
+import { get, set } from 'idb-keyval';
+import { useAuthStore } from '../stores/authStore';
+
+import * as agent from "@dfinity/agent";
+
+// Usage of the imported bindings only works if the respective .wasm was loaded, which is done in main.ts.
+// See also https://github.com/rollup/plugins/tree/master/packages/wasm#using-with-wasm-bindgen-and-wasm-pack
+import * as vetkd from "ic-vetkd-utils-wasm2js";
+
+export class CryptoService {
+  constructor(private actor: BackendActor) {
+  }
+
+  // The function encrypts data with the secret-id-specific secretKey.
+  public async encryptWithSecretKey(secret_id: bigint, owner: string, data: string) {
+    await this.fetch_secret_key_if_needed(secret_id, owner);
+    const secret_key = await this.getSecretKey(secret_id, owner);
+
+    const data_encoded = Uint8Array.from([...data].map(ch => ch.charCodeAt(0))).buffer
+    // The iv must never be reused with a given key.
+    const iv = window.crypto.getRandomValues(new Uint8Array(12));
+    const ciphertext = await window.crypto.subtle.encrypt(
+      {
+        name: "AES-GCM",
+        iv: iv
+      },
+      secret_key,
+      data_encoded
+    );
+
+    const iv_decoded = String.fromCharCode(...new Uint8Array(iv));
+    const cipher_decoded = String.fromCharCode(...new Uint8Array(ciphertext));
+    return iv_decoded + cipher_decoded;
+  }
+
+  // The function decrypts the given input data with the secret-id-specific secretKey.
+  public async decryptWithSecretKey(secret_id: bigint, owner: string, data: string) {
+    await this.fetch_secret_key_if_needed(secret_id, owner);
+    const secret_key = await this.getSecretKey(secret_id, owner);
+    if (data.length < 13) {
+      throw new Error('wrong encoding, too short to contain iv');
+    }
+    const iv_decoded = data.slice(0, 12);
+    const cipher_decoded = data.slice(12);
+    const iv_encoded = Uint8Array.from([...iv_decoded].map(ch => ch.charCodeAt(0))).buffer;
+    const ciphertext_encoded = Uint8Array.from([...cipher_decoded].map(ch => ch.charCodeAt(0))).buffer;
+
+    let decrypted_data_encoded = await window.crypto.subtle.decrypt(
+      {
+        name: "AES-GCM",
+        iv: iv_encoded
+      },
+      secret_key,
+      ciphertext_encoded
+    );
+    const decrypted_data_decoded = String.fromCharCode(...new Uint8Array(decrypted_data_encoded));
+    return decrypted_data_decoded;
+  }
+
+  private async fetch_secret_key_if_needed(secret_id: bigint, owner: string) {
+    //console.log("fetch_secret_key_if_needed");
+    if (!await get([secret_id.toString(), owner])) {
+      const seed = window.crypto.getRandomValues(new Uint8Array(32));
+      //console.log("TransportSecretKey");
+      const tsk = new vetkd.TransportSecretKey(seed);
+
+      //console.log("end TransportSecretKey");
+      const ek_bytes_hex = await this.actor.encrypted_symmetric_key_for_secret(secret_id, tsk.public_key());
+      //console.log("end encrypted_symmetric_key_for_secret");
+      const pk_bytes_hex = await this.actor.symmetric_key_verification_key_for_secret();
+      //console.log("end symmetric_key_verification_key_for_secret");
+      // const secret_id_bytes: Uint8Array = bigintTo128BitBigEndianUint8Array(secret_id);
+      // const owner_utf8: Uint8Array = new TextEncoder().encode(owner);
+      // let derivation_id = new Uint8Array(secret_id_bytes.length + owner_utf8.length);
+      // derivation_id.set(secret_id_bytes);
+      // derivation_id.set(owner_utf8, secret_id_bytes.length);
+      // Get the principal from the auth client
+      const authStore = useAuthStore();
+      if (!authStore.client) {
+        throw new Error("Auth client not initialized");
+      }
+      const derivation_id = authStore.client.getIdentity().getPrincipal().toUint8Array();
+      //console.log("end derivation_id");
+
+      //tsk.decrypt_and_hash でエラーになっている。index-B6gj0x33.js:13 invalid encrypted key: verification failed
+      const aes_256_gcm_key_raw = tsk.decrypt_and_hash(
+        hex_decode(ek_bytes_hex),
+        hex_decode(pk_bytes_hex),
+        derivation_id,
+        32,
+        new TextEncoder().encode("aes-256-gcm")
+      );
+      //console.log("end decrypt_and_hash");
+      //console.log("start importKey");
+      const secret_key: CryptoKey = await window.crypto.subtle.importKey("raw", aes_256_gcm_key_raw, "AES-GCM", false, ["encrypt", "decrypt"]);
+      
+      //console.log("end importKey");
+      //console.log(secret_key);
+      await set([secret_id.toString(), owner], secret_key)
+    }
+  }
+
+  private async getSecretKey(secret_id: bigint, owner: string): Promise<CryptoKey> {
+    const storedKey = await get([secret_id.toString(), owner]);
+    if (!storedKey) {
+      throw new Error(`No secret key found for secret_id: ${secret_id} and owner: ${owner}`);
+    }
+    return storedKey as CryptoKey;
+  }
+}
+
+const hex_decode = (hexString: string): Uint8Array =>
+  Uint8Array.from(hexString.match(/.{1,2}/g)?.map((byte) => parseInt(byte, 16)) || []);
+
+const hex_encode = (bytes: Uint8Array): string =>
+  bytes.reduce((str, byte) => str + byte.toString(16).padStart(2, '0'), '');
+
+// Inspired by https://coolaj86.com/articles/convert-js-bigints-to-typedarrays/
+function bigintTo128BitBigEndianUint8Array(bn: bigint): Uint8Array {
+  var hex = BigInt(bn).toString(16);
+
+  // extend hex to length 32 = 16 bytes = 128 bits
+  while (hex.length < 32) {
+    hex = '0' + hex;
+  }
+
+  var len = hex.length / 2;
+  var u8 = new Uint8Array(len);
+
+  var i = 0;
+  var j = 0;
+  while (i < len) {
+    u8[i] = parseInt(hex.slice(j, j + 2), 16);
+    i += 1;
+    j += 2;
+  }
+
+  return u8;
+}
---
+++ dev-icp-hackathon/icpassproj/src/icpassproj_frontend/src/stores/authStore.ts
@@ -0,0 +1,135 @@
+import { defineStore } from 'pinia';
+import { createActor, type BackendActor } from '../libs/actor';
+import { AuthClient } from '@dfinity/auth-client';
+import { CryptoService } from '../libs/crypto';
+import type { JsonnableDelegationChain } from '@dfinity/identity/lib/cjs/identity/delegation';
+
+// import 'dotenv/config'
+
+export type AuthState =
+  | 'initializing-auth'
+  | 'anonymous'
+  | 'initializing-crypto'
+  | 'synchronizing'
+  | 'initialized'
+  | 'error';
+
+export const useAuthStore = defineStore('auth', {
+  state: () => ({
+    state: 'initializing-auth' as AuthState,
+    actor: null as BackendActor | null,
+    client: null as AuthClient | null,
+    crypto: null as CryptoService | null,
+    error: '' as string
+  }),
+
+  getters: {
+    isAuthenticated: (state) => state.state === 'initialized',
+    isAnonymous: (state) => state.state === 'anonymous',
+    isInitializing: (state) => state.state === 'initializing-auth' || state.state === 'initializing-crypto'
+  },
+
+  actions: {
+    async initAuth() {
+      const client = await AuthClient.create();
+      this.client = client;
+      
+      if (await client.isAuthenticated()) {
+        this.authenticate();
+      } else {
+        this.state = 'anonymous';
+        this.actor = createActor();
+      }
+    },
+
+    async login() {
+      if (this.state !== 'anonymous' || !this.client) return;
+
+      const identityProvider = getIdentityProvider();
+
+      this.client.login({
+        maxTimeToLive: BigInt(1800) * BigInt(1_000_000_000),
+        identityProvider,
+        onSuccess: () => {this.authenticate()},
+      });
+    },
+
+   
+
+    async logout() {
+      if (this.state !== 'initialized' || !this.client) return;
+      
+      await this.client.logout();
+      this.state = 'anonymous';
+      this.actor = createActor();
+      this.crypto = null;
+      // router.push('/') // 必要に応じてルーターを使用
+    },
+
+    async authenticate() {
+      //console.log("authenticate");
+      const client = this.client!;
+      this.handleSessionTimeout();
+      //console.log("handleSessionTimeout");
+
+      try {
+        const actor = createActor({
+          agentOptions: {
+            identity: client.getIdentity(),
+          },
+        });
+
+        //console.log("actor");
+        this.state = 'initializing-crypto';
+        this.actor = actor;
+        
+        const cryptoService = new CryptoService(actor);
+        
+        this.state = 'initialized';
+        this.crypto = cryptoService;
+      } catch (e) {
+        this.state = 'error';
+        this.error = 'An error occurred';
+        console.error("Error", e);
+
+      }
+    },
+
+    handleSessionTimeout() {
+      // upon login the localstorage items may not be set, wait for next tick
+      setTimeout(() => {
+        try {
+          const delegation = JSON.parse(
+            localStorage.getItem('ic-delegation') || '"test"'
+          ) as JsonnableDelegationChain;
+
+          const expirationTimeMs =
+            Number.parseInt(delegation.delegations[0].delegation.expiration, 16) /
+            1000000;
+
+          setTimeout(() => {
+            this.logout();
+          }, expirationTimeMs - Date.now());
+        } catch {
+          console.error('Could not handle delegation expiry.');
+        }
+      });
+    }
+  }
+});
+
+export function getIdentityProvider(): string {
+  if(process.env.DFX_NETWORK === "local") {// Safari detection
+    const isSafari = /^(?!.*chrome\/\d+)(?!.*chromium\/\d+).*safari\/\d+/i.test(navigator.userAgent);        
+    return isSafari ? `http://localhost:4943/?canisterId=${process.env.CANISTER_ID_INTERNET_IDENTITY}` : `http://${process.env.CANISTER_ID_INTERNET_IDENTITY}.localhost:4943`;
+  } else if (process.env.DFX_NETWORK === "ic") {
+    return `https://identity.ic0.app/#authorize`;  //NOTE: 本番環境は必ずこのURLでなければならない
+  } else {
+    return `https://${process.env.CANISTER_ID_INTERNET_IDENTITY}.dfinity.network`;
+  }
+}
+
+// 初期化は通常 main.ts または App.vue の setup で行う
+// ここでは参考として示す
+// const authStore = useAuthStore();
+// authStore.initAuth();
